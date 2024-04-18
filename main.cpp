c196c96df2a698793730619d1901c91803addf78
Rayan ANKI <ranki@paul-f4Ar5s11.clusters.42paris.fr>
2024-04-18 20:35:11 +0200

    final push

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ranki <ranki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:51:00 by ranki             #+#    #+#             */
-/*   Updated: 2024/04/18 16:36:20 by ranki            ###   ########.fr       */
+/*   Updated: 2024/04/18 20:33:10 by ranki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,15 @@ void signalHandler(int signum) {
 
 int main(int argc, char **argv)
 {
-    // if (argc != 3) {
-    //     usage();
-    //     return 1;
-    // }
-    (void)argc;
-    (void)argv;
+    if (argc != 3) {
+        usage();
+        return 1;
+    }
     std::cout << std::endl
               << GRE << "*** SERVER *** \n"
               << WHI << std::endl;
-    std::string password = "1"; // normalement donner par l'user  argv[2]
-    const char *port = "6667";  // normalement donner par l'user  argv[1]
+    std::string password = argv[2]; // normalement donner par l'user  
+    const char *port = argv[1];  // normalement donner par l'user  
     if (argc == 3)
     {
         password = argv[2];
--- src/CommunicationClient.cpp
+++ src/CommunicationClient.cpp
@@ -6,7 +6,7 @@
 /*   By: ranki <ranki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 17:51:20 by ranki             #+#    #+#             */
-/*   Updated: 2024/04/18 17:00:11 by ranki            ###   ########.fr       */
+/*   Updated: 2024/04/18 20:26:24 by ranki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,14 @@ void Server::receiveData(int fd)
 		}
 		
 		std::cout << "Received in Client : " << cli->getBuffer() << std::endl;
+		std::string buffer2 = cli->getBuffer();
+		while (buffer2.find("\n") != std::string::npos)
+		{
+			buffer2 = buffer2.substr(0, buffer2.find("\n")) + buffer2.substr(buffer2.find("\n") + 1, buffer2.size());
+		}
+		buffer2 += "\n";
 
-		std::istringstream iss(buffer);
+		std::istringstream iss(buffer2);
 		std::string line;
 		while (std::getline(iss, line))
 		{
